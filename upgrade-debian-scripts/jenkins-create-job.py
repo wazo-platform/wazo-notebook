8bf62e5b1ffb70e8007be2eed05684919bde6195
Francois Blackburn <fblackburn@wazo.community>
2025-09-05 13:49:45 -0400

    Update debian 11 script to be more generic

--- upgrade-debian-scripts/jenkins-create-job.py
+++ upgrade-debian-scripts/jenkins-create-job.py
@@ -7,7 +7,7 @@ import jenkins
 from lxml import etree
 
 server = jenkins.Jenkins(
-    os.getenv("JENKINS_URL"),
+    os.getenv("JENKINS_URL", ""),
     username=os.getenv("JENKINS_USER_ID"),
     password=os.getenv("JENKINS_API_TOKEN"),
 )
@@ -16,18 +16,19 @@ version = server.get_version()
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-d", '--debian', action="store", required=True)
 parser.add_argument("--doit", action="store_true")
 parser.add_argument("project")
 args = parser.parse_args()
 
 master = args.project
-bullseye = f'{args.project}-bullseye'
+job_name = f'{args.project}-{args.debian}'
 
 xml_raw = server.get_job_config(master)
 xml = etree.fromstring(xml_raw.encode())
 
 branch = xml.xpath('definition/scm/branches/hudson.plugins.git.BranchSpec/name')[0]
-branch.text = "bullseye"
+branch.text = args.debian
 
 print(f'* branch: {branch.text}')
 print('')
@@ -35,5 +36,5 @@ print('')
 
 if args.doit:
     output = etree.tostring(xml)
-    server.create_job(bullseye, output.decode())
-    server.build_job(bullseye)
+    server.create_job(job_name, output.decode())
+    server.build_job(job_name)
---
+++ upgrade-debian-scripts/update-jenkinsfile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -u  # fail if variable is undefined
+set -o pipefail  # fail if command before pipe fails
+
+DEBIAN_CODENAME="${1}"
+REPO_NAME="${2}"
+JENKINSFILE="${LOCAL_GIT_REPOS}/${REPO_NAME}/Jenkinsfile"
+
+# Build debian packaging on bookworm distribution
+sed -i "s/value: \"\${JOB_NAME}\"/value: \"${REPO_NAME}\"/" "${JENKINSFILE}"
+sed -i "/.*PACKAGE.*/a \          string(name: \"BRANCH\", value: \"${DEBIAN_CODENAME}\")," "${JENKINSFILE}"
+sed -i "/.*BRANCH.*/a \          string(name: \"DISTRIBUTION\", value: \"wazo-dev-${DEBIAN_CODENAME}\")," "${JENKINSFILE}"
+
+# Tag docker image with bookworm
+sed -i "s/:latest/:${DEBIAN_CODENAME}/g" "${JENKINSFILE}"
